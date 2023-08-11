4d2ff97e0dfd8d7a38c570757938b1bf51cb2536
Hartaithan <hartaithan@gmail.com>
2023-08-11 17:38:55 +0900

    install font and add basic styling

--- src/app/pages/character/character.component.ts
+++ src/app/pages/character/character.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-character',
+  selector: 'app-character-page',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss'],
 })
